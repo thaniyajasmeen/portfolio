228975ca9b56cfc35f84a120df96eb0a16e216e6
sohail <sohail@sohails-MacBook-Air.local>
2024-10-30 13:28:15 +0530

    first commit

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -25,7 +25,7 @@ export default function Header({ darkModeEnabled, toggleMode }) {
             className="text-md cursor-pointer font-lato font-semibold tracking-widest md:text-2xl"
             onClick={jumpToTop}
           >
-            &lt;DhavalPatel /&gt;
+            &lt;Thaniya Jasmeen /&gt;
           </h3>
         </motion.div>
         <motion.div
