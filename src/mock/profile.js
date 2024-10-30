07f6fd31ebe11ffbd8fdcac9b1724f6a8bc053b7
Mohammed Mudabbir Pasha <116947092+mudabbir525@users.noreply.github.com>
2024-10-30 16:54:38 +0530

    Update profile.js

--- src/mock/profile.js
+++ src/mock/profile.js
@@ -11,8 +11,7 @@ export const SOCIAL_LINKS = [
 
 export const HERO_TITLES = ['Thaniya Jasmeen', 'Business Analyst'];
 
-export const aboutParagraph =
-  'Highly analytical and detail-oriented Business Analyst and Power BI professional with a strong foundation in data visualization, statistical analysis, and data-driven decision-making. Skilled in transforming complex data into actionable insights using tools such as Power BI, SQL, Excel, and Python. Proficient in building interactive dashboards, performing data cleansing, and leveraging advanced data modeling techniques to support business objectives and enhance performance metrics. Adept at communicating insights to both technical and non-technical stakeholders to drive strategic initiatives and optimize operational efficiency. Passionate about uncovering trends and patterns to inform impactful business decisions.';
+export const aboutParagraph = 'Highly analytical and detail-oriented Business Analyst and Power BI professional with a strong foundation in data visualization, statistical analysis, and data-driven decision-making. Skilled in transforming complex data into actionable insights using tools such as Power BI, SQL, Excel, and Python. Proficient in building interactive dashboards, performing data cleansing, and leveraging advanced data modeling techniques to support business objectives and enhance performance metrics. Adept at communicating insights to both technical and non-technical stakeholders to drive strategic initiatives and optimize operational efficiency. Passionate about uncovering trends and patterns to inform impactful business decisions.';
 
 export const contactEmail = 'jazzthaniya51@gmail.com';
 
